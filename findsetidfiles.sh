831544be6a33b8c85121d315c596db32d545c2ba
jaime.e.laguna@gmail.com <Mycllg7681>
2021-02-04 15:34:47 -0500

    I cant believe that for a line so many changes

--- findsetidfiles.sh
+++ findsetidfiles.sh
@@ -5,5 +5,5 @@ echo "Jaime Laguna filter:"
 echo "Display only file name, owner, and size of 12 largest files which complies with the executable attribute in the entire disk"
 echo "============================================================================================================================"
 echo "File name                                                  Owner       Size"
-find / -type f -exec ls -lh {} + 2>/dev/null | sort -k 5 -h | tail -n 12 | awk '{print $9,":",$4,":",$5}'| column -t
+find / -type f -exec ls -lh {} + 2>/dev/null | sort -k 5 -h | tail -n 12 | awk '{print $9,":",$3,":",$5}'| column -t
 echo ""
